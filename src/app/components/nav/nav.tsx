37b8bfe62bfc1dbadbae77dce2b413d315e65f04
guido morabito <guidomorabito161@hotmail.com>
2024-08-10 18:00:52 -0300

    video

--- src/app/components/nav/nav.tsx
+++ src/app/components/nav/nav.tsx
@@ -9,7 +9,7 @@ const menuItems = [
 
 const Nav = () => {
   return (
-    <div className='h-[5vh] bg-custom-violet flex items-center justify-center'>
+    <div className='h-[5vh] w-[100vw] bg-custom-violet flex items-center justify-center z-10 fixed'>
       {menuItems.map(({title, path}) => 
         <NavItems title={title} path={path} />
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
   return (
     <>
       <div>
-
-        Floreria
+        <div className="w-[100%] h-[100vh] overflow-hidden">
+          <video src={require('../../public/recorte.mp4')} autoPlay muted loop />
+        </div>
       </div>
+
     </>
   );
 }
