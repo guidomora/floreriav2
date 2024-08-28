9b32f9fd15bb93519e92922f13c2317ed308252a
guido morabito <guidomorabito161@hotmail.com>
2024-08-28 12:38:08 -0300

    meta 2

--- src/app/categoria/[id]/page.tsx
+++ src/app/categoria/[id]/page.tsx
@@ -35,17 +35,9 @@ export async function generateMetadata(
   // read route params
   const id = params.id
 
-  // fetch data
-  // const product = await fetch(`https://.../${id}`).then((res) => res.json())
- 
-  // optionally access and extend (rather than replace) parent metadata
-  // const previousImages = (await parent).openGraph?.images || []
  
   return {
     title: `${id} Floreria Morabito`,
-    // openGraph: {
-    //   images: ['/some-specific-page-image.jpg', ...previousImages],
-    // },
     description: descriptionSelector(id),
   }
 }
